2bb7ae6fbe0c9bd6fc0259b938f8d818214aee74
WillyMaikowski <willy.maikowski@gmail.com>
2014-08-21 15:21:40 -0400

    Listo y probado - falta optimizacion

--- digitRecognition/__init__.py
+++ digitRecognition/__init__.py
@@ -3,41 +3,36 @@ import cv2
 import utils
 import os
 
+#constantes
+TRAIN = 0
+TEST = 1
 FEATURE = 0
 CLASS = 1
 
-imgs_train = [ [], [] ]
+imgs = [ [ [], [] ], [ [], [] ] ]
 for i in range( 10 ):
-    for r, d, f in os.walk( "digitos/train/digit_" + str( i ) ):
-        for img in f:
-            img = cv2.imread( img, 0 )  
-            ret, img = cv2.threshold( img, 127, 255, cv2.THRESH_BINARY )
-            img_feature = utils.apply4CC(img) #extraccion del histograma, deberia ser una funcion de img
-            hist = cv2.calcHist([img_feature], [0], None,[16],[0,16])
-            imgs_train[FEATURE].append( hist )#histograma
-            imgs_train[CLASS].append( i )#clase
-        
-
-imgs_test = [ [],[] ]
-for i in range( 10 ):
-    for r, d, f in os.walk( "digitos/test/digit_" + str( i ) ):
-        for img in f:
-            img = cv2.imread( img, 0 )  
-            ret, img = cv2.threshold( img, 127, 255, cv2.THRESH_BINARY )
-            img_feature = utils.apply4CC(img) #extraccion del histograma, deberia ser una funcion de img
-            hist = cv2.calcHist([img_feature], [0], None,[16],[0,16])
-            imgs_test[FEATURE].append( hist )#histograma
-            imgs_test[CLASS].append( i )#clase 
-
+    path = [ "digitos/train/digit_" + str( i ) + "/", "digitos/test/digit_" + str( i ) + "/" ]
+    for j in range( 2 ):
+        for r, d, f in os.walk( path[j] ):
+            for img in f:
+                img = cv2.imread( path[j] + img, 0 )
+                ret, img = cv2.threshold( img, 127, 255, cv2.THRESH_BINARY )
+                img_feature = utils.apply4CC( img )  # extraccion del histograma, deberia ser una funcion de img
+                hist = cv2.calcHist( [img_feature], [0], None, [16], [0, 16] )
+                imgs[j][FEATURE].append( hist )  # histograma
+                imgs[j][CLASS].append( i )  # clase
 
 knn = cv2.KNearest()
 # Training
-knn.train( imgs_train[FEATURE], imgs_train[CLASS] )
+knn.train( imgs[TRAIN][FEATURE], imgs[TRAIN][CLASS] )
 
-'''
-#Prediction
-k = 5 #Debemos calcular k
-retval, results, neigh_resp, dists = knn.find_nearest( imgs_test, k )
-#results.ravel() ?
+#guardar entrenamiento o calculos antes del entrenamiento
 
-'''
+# Prediction
+k = 5  # Debemos calcular k
+retval, results, neigh_resp, dists = knn.find_nearest( imgs[TEST][FEATURE], k )
+
+matches = results == imgs[TEST][CLASS]
+correct = np.count_nonzero( matches )
+accuracy = correct * 100.0 / results.size
+print accuracy
--- digitRecognition/utils.py
+++ digitRecognition/utils.py
@@ -1,3 +1,11 @@
+
+# -------------------------------------------------
+# Falta optimizar estos metodos, toman mucho tiempo. 
+# Quizas sea bueno hacerlo multi core para aprovechar
+# tu pc. Se me ocurrio una forma facil de hacerla y
+# tomaria tiempo O(n). Ahi vemos si hay una mejor forma!
+# -------------------------------------------------
+
 def hasBlackLeft( img, i, j ):
     if j <= 0:
         return False
@@ -70,74 +78,83 @@ def hasBlackBottomRight( img, i, j ):
             return True
     return False
 
-#retorna true si hay una salida por s1
-def hasS1(img, i, j):
+# retorna true si hay una salida por s1
+def hasS1( img, i, j ):
     if i <= 1:
         return False
-    #se asume que el pixel esta rodeado en toda direccion
-    for k in range(i-1,-1,-1):
-        if img[k, j] == 0:#negro -> dejar de buscar
+    # se asume que el pixel esta rodeado en toda direccion
+    for k in range( i - 1, -1, -1 ):
+        if img[k, j] == 0:  # negro -> dejar de buscar
             return False
-        if not hasBlackLeft(img, k, j):
+        if not hasBlackLeft( img, k, j ):
             return True
     return False
 
-#retorna true si hay una salida por s2
-def hasS2(img, i, j):
+# retorna true si hay una salida por s2
+def hasS2( img, i, j ):
     if i <= 1:
         return False
-    #se asume que el pixel esta rodeado en toda direccion
-    for k in range(i-1,-1,-1):
-        if img[k, j] == 0:#negro -> dejar de buscar
+    # se asume que el pixel esta rodeado en toda direccion
+    for k in range( i - 1, -1, -1 ):
+        if img[k, j] == 0:  # negro -> dejar de buscar
             return False
-        if not hasBlackRight(img, k, j):
+        if not hasBlackRight( img, k, j ):
             return True
     return False
 
-#retorna true si hay una salida por s3
-def hasS3(img, i, j):
-    if i >= len(img)-1:
+# retorna true si hay una salida por s3
+def hasS3( img, i, j ):
+    if i >= len( img ) - 1:
         return False
-    #se asume que el pixel esta rodeado en toda direccion
-    for k in range(i+1, len(img)):
-        if img[k, j] == 0:#negro -> dejar de buscar
+    # se asume que el pixel esta rodeado en toda direccion
+    for k in range( i + 1, len( img ) ):
+        if img[k, j] == 0:  # negro -> dejar de buscar
             return False
-        if not hasBlackLeft(img, k, j):
+        if not hasBlackLeft( img, k, j ):
             return True
     return False
 
-#retorna true si hay una salida por s4
-def hasS4(img, i, j):
-    if i >= len(img)-1:
+# retorna true si hay una salida por s4
+def hasS4( img, i, j ):
+    if i >= len( img ) - 1:
         return False
-    #se asume que el pixel esta rodeado en toda direccion
-    for k in range(i+1, len(img)):
-        if img[k, j] == 0:#negro -> dejar de buscar
+    # se asume que el pixel esta rodeado en toda direccion
+    for k in range( i + 1, len( img ) ):
+        if img[k, j] == 0:  # negro -> dejar de buscar
             return False
-        if not hasBlackRight(img, k, j):
+        if not hasBlackRight( img, k, j ):
             return True
     return False
 
 
 def apply4CC( img ):
     aux = img.copy()
-    for i in range(len(img[0])):
-        for j in range(len(img)):
-            aux[i,j] = m4CC(img, i, j)
+    for i in range( len( img[0] ) ):
+        for j in range( len( img ) ):
+            if img[i, j] == 0:
+                aux[i, j] = -1
+            else:
+                aux[i, j] = m4CC( img, i, j )
     return aux
 
 def apply8CC( img ):
     aux = img.copy()
-    for i in range(len(img[0])):
-        for j in range(len(img)):
-            aux[i,j] = m8CC(img, i, j)
+    for i in range( len( img[0] ) ):
+        for j in range( len( img ) ):
+            if img[i, j] == 0:
+                aux[i, j] = -1
+            else:
+                aux[i, j] = m8CC( img, i, j )
     return aux
 
 def apply13C( img ):
     aux = img.copy()
-    for i in range(len(img[0])):
-        for j in range(len(img)):
-            aux[i,j] = m13C(img, i, j)
+    for i in range( len( img[0] ) ):
+        for j in range( len( img ) ):
+            if img[i, j] == 0:
+                aux[i, j] = -1
+            else:
+                aux[i, j] = m13C( img, i, j )
     return aux
 
 def m13C( img, i, j ):
@@ -180,34 +197,34 @@ def m13C( img, i, j ):
     # num_negros == 4 =>encontrar direccion
     if hasS1:
         return 9
-    
+
     if hasS2:
         return 10
-    
+
     if hasS3:
         return 11
-    
+
     if hasS4:
         return 12
-    
-    return 8#punto interior
+
+    return 8  # punto interior
 
 def m4CC( img, i, j ):
-    right = hasBlackRight( img, i, j )  
-    left = hasBlackLeft( img, i, j )  
+    right = hasBlackRight( img, i, j )
+    left = hasBlackLeft( img, i, j )
     top = hasBlackTop( img, i, j )
     bottom = hasBlackBottom( img, i, j )
-    
-    return int(top)<<3|int(right)<<2|int(bottom)<<1|int(left)
+
+    return int( top ) << 3 | int( right ) << 2 | int( bottom ) << 1 | int( left )
 
 def m8CC( img, i, j ):
-    right = hasBlackRight( img, i, j )  
-    left = hasBlackLeft( img, i, j )  
+    right = hasBlackRight( img, i, j )
+    left = hasBlackLeft( img, i, j )
     top = hasBlackTop( img, i, j )
     bottom = hasBlackBottom( img, i, j )
     topLeft = hasBlackTopLeft( img, i, j )
     bottomLeft = hasBlackBottomLeft( img, i, j )
     bottomRight = hasBlackBottomRight( img, i, j )
     topRight = hasBlackTopRight( img, i, j )
-    
-    return int(top)<<7|int(topRight)<<6|int(right)<<5|int(bottomRight)<<4|int(bottom)<<3|int(bottomLeft)<<2|int(left)<<1|int(topLeft)
+
+    return int( top ) << 7 | int( topRight ) << 6 | int( right ) << 5 | int( bottomRight ) << 4 | int( bottom ) << 3 | int( bottomLeft ) << 2 | int( left ) << 1 | int( topLeft )
